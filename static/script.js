a390f8b80178d632f8c6c55ae42f1da1d5d1c459
ZheQing <chaizheqing2004@gmail.com>
2025-03-22 19:27:49 +0800

    example of weather data (but need change)

--- static/script.js
+++ static/script.js
@@ -4,23 +4,32 @@ document.addEventListener("DOMContentLoaded", () => {
   .then(response => response.json())
   .then(data => {
     console.log(data);
+    console.log(Object.values(data["location"]))
     // Build your Chart.js chart here
     const ctx = document.getElementById("barChart");
 
     new Chart(ctx, {
       type: "bar",
       data: {
-        labels: [Object.values(data["location"])],
+        labels: Object.values(data["location"]),
         datasets: [
           {
             label: "AirTemp",
-            data: data["airTemp"],
+            data: Object.values(data["airTemp"]),
             borderWidth: 1,
           },
         ],
       },
       options: {
         scales: {
+          x: {
+            ticks: {
+              autoSkip: false,
+              maxRotation: 90,
+              minRotation: 90
+            }
+          },
+        
           y: {
             beginAtZero: true,
           },
